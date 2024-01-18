d8fe7e25fb0623f6e50780e1eadb520272249f98
Sawyereva <106629658+Sawyereva@users.noreply.github.com>
2024-01-17 22:23:20 -0700

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpFun
+{
+    internal class Class1
+    {
+        public static int[] Roller(int rollnum)
+        {
+            int die1 = 0;
+            int die2 = 0;
+            int roll = 0;
+            int[] counts = new int[11];
+            for (int i = 0; i < rollnum; i++)
+            {
+                Random rnd = new Random();
+                die1 = rnd.Next(1, 7);
+                die2 = rnd.Next(1, 7);
+                roll = die1 + die2;
+
+                counts[roll - 2]++;
+            }
+            return counts;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Threading.Tasks;
+using CSharpFun;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+
+        int[] rollcounts = new int[11];
+        System.Console.WriteLine("How many times would you like to roll the dice?");
+        string temp = System.Console.ReadLine();
+        int times = int.Parse(temp);
+
+        rollcounts = Class1.Roller(times);
+
+        double percent = 0;
+        System.Console.WriteLine("DICE ROLLING SIMULATION RESULTS");
+        System.Console.WriteLine("Each  *  represents 1% of the total number of rolls.");
+        System.Console.WriteLine("Total number of rolls = " + times);
+        for (int i = 2; i < 13; i++)
+        {
+            percent = rollcounts[i-2] * 100 / times;
+            string output = "";
+            for (int iInner = 0; iInner < percent; iInner++)
+            {
+                output = output + "*";
+            }
+            System.Console.WriteLine(i + ":\t" + output);
+            }
+        System.Console.WriteLine("Thank you for using the dice throwing simulator. Goodbye!");
+    }
+
+    
+}
